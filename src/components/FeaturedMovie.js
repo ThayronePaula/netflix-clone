23eb440d717beeb5af23a416a67224baad1e99d4
thayrone <thayronedepaula15@gmail.com>
2021-07-08 21:04:36 -0300

    finished banner featured

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import './FeaturedMovie.css'
+import React from "react";
+import "./FeaturedMovie.css";
 
-const FeaturedMovie = ({item}) => {
+const FeaturedMovie = ({ item }) => {
+  console.log(item);
   return (
-    <section className='featured'  >
-     {item.name}
-    </section>
-  )
-}
+    <section
+      className="featured"
+      style={{
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+        background: ` url('https://image.tmdb.org/t/p/original${item.backdrop_path}') no-repeat`,
+      }}
+    >
+      <div className="featured--vertical">
+        <div className="featured--horizontal">
+          <div className="featured--about">
+            <h1>{item.name}</h1>
+            <div className="featured--info">
+              <span>{item.vote_average} pontos</span>
+              <span>{item.first_air_date.slice(0,4)}</span>
+              <span>{ item.number_of_seasons} { item.number_of_seasons > 1 ? 'temporadas' : 'temporada' }</span>
+            </div>
+            <div className="featured--info">
 
-export default FeaturedMovie
+              {item.overview}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedMovie;
